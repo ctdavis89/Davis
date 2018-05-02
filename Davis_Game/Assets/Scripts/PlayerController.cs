4efddba3c6d1323f552a03226316f41f40911738
chris Davis <36212638+ctdavis89@users.noreply.github.com>
2018-05-01 20:09:36 -0400

    working on grapple

--- Davis_Game/Assets/Scripts/PlayerController.cs
+++ Davis_Game/Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour {
 
     private Collider2D myCollider;
 
+    RaycastHit _hit;
+    ContactFilter2D _filter;
 
 	// Use this for initialization
 	void Start () {
@@ -37,6 +39,29 @@ public class PlayerController : MonoBehaviour {
             {
                 myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpforce);
             }
+            else
+            {
+                Grapple();
+            }
         }
 	}
+
+    void Grapple()
+    {
+
+        if(Physics2D.Raycast(transform.position, transform.up, 1000 _filter ,_hits))
+        {
+            
+
+
+        }
+        if (Physics.Raycast(transform.position, transform.up, out _hit, 1000))
+        {
+
+            
+        }
+
+
+    }
+
 }
